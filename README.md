93f3204d6969e78a93836cef9c045e203de64960
Aaron Escoboza Villegas <aaron.villegas@hotmail.com>
2021-06-17 23:33:57 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## **Description**
+
+Lcd 16x2 diver for STM32F4x microcontrollers based on I2C and HAL layer.  
---
+++ driver/lcd1602_i2c.h
@@ -0,0 +1,20 @@
+/*
+*       Author: Aarón Escoboza Villegas
+*       Github: aaron-ev
+*
+**/
+
+#ifndef LCD1602_I2C_H
+#define LCD1602_I2C_H
+
+// macro definition
+
+// function prototypes
+
+/*
+*   function name:
+*     description:
+*           input:
+*/
+
+#endif
